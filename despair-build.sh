039acc2238a5550e1359123b628186dc851ee782
ripng <matthewalex@outlook.com>
2015-03-17 01:15:26 -0400

    build: tc change to sabernaro

--- despair-build.sh
+++ despair-build.sh
@@ -21,7 +21,7 @@ AK_VER="$BASE_AK_VER$VER"
 
 # Vars
 export LOCALVERSION=~`echo $AK_VER`
-export CROSS_COMPILE=${HOME}/android/ubertc/bin/arm-eabi-
+export CROSS_COMPILE=${HOME}/android/sabernaro/bin/arm-eabi-
 export ARCH=arm
 export SUBARCH=arm
 export KBUILD_BUILD_USER=DespairFactor
