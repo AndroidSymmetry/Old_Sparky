a412f0626f09a80fc8f4c95605ba28ed83d21f04
despairfactor <matthewalex@outlook.com>
2015-03-28 14:52:40 -0400

    fs: support task's adj rbtree
    Add (or del) a task to (or from) task's adj rbtree
    when its oom_score_adj is modified.
    
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Signed-off-by: Yi-wei Zhao <gbjc64@motorola.com>
    Reviewed-on: http://gerrit.mot.com/701206
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -654,10 +654,10 @@ int setup_arg_pages(struct linux_binprm *bprm,
 	unsigned long rlim_stack;
 
 #ifdef CONFIG_STACK_GROWSUP
-	/* Limit stack size to 1GB */
+	/* Limit stack size */
 	stack_base = rlimit_max(RLIMIT_STACK);
-	if (stack_base > (1 << 30))
-		stack_base = 1 << 30;
+	if (stack_base > STACK_SIZE_MAX)
+		stack_base = STACK_SIZE_MAX;
 
 	/* Make sure we didn't let the argument array grow too large. */
 	if (vma->vm_end - vma->vm_start > stack_base)
@@ -952,6 +952,7 @@ static int de_thread(struct task_struct *tsk)
 		transfer_pid(leader, tsk, PIDTYPE_SID);
 
 		list_replace_rcu(&leader->tasks, &tsk->tasks);
+#ifdef CONFIG_ANDROID_LMK_ADJ_RBTREE
 		/*
 		 * need to delete leader from adj tree, because it will not be
 		 * group leader (exit_signal = -1) soon. release_task(leader)
@@ -959,6 +960,7 @@ static int de_thread(struct task_struct *tsk)
 		 */
 		delete_from_adj_tree(leader);
 		add_2_adj_tree(tsk);
+#endif
 		list_replace_init(&leader->sibling, &tsk->sibling);
 
 		tsk->group_leader = tsk;
